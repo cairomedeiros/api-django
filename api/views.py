324035bdd8f7a2ca2917a753c35d81336e6e598f
cairomedeiros <cairomedeiros13@gmail.com>
2024-09-25 21:55:56 -0300

    starting a viewset

--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 from apidjango.mongodb import MongoConnection
-# Create your views here.
-def index(request):
-    db, client = MongoConnection.get_db_handle("teste")
-    return HttpResponse("<h1>Hello World!</h1>")
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+class PandasViewSet(viewsets.ViewSet):
+    def create(self, request):
+        db, client = MongoConnection.get_db_handle("teste")
+        return Response("<h1>Hello World!</h1>", status=status.HTTP_200_OK)
