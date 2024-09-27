a882d88062ee51be2a5abb04515687060cc489f5
cairomedeiros <cairomedeiros13@gmail.com>
2024-09-26 22:07:03 -0300

    add python-dotenv and configuring some variables

--- apidjango/mongodb.py
+++ apidjango/mongodb.py
@@ -1,12 +1,13 @@
 from pymongo import MongoClient
+from . import settings
 
 class MongoConnection(object):
 
     def get_db_handle(db_name):
         client = MongoClient(
                             port=int("27017"),
-                            username="root",
-                            password="root"
+                            username=settings.MONGODB_USERNAME,
+                            password=settings.MONGODB_PASSWORD
                             )
         db_handle = client[db_name]
         return db_handle, client
