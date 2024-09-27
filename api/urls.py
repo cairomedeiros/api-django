5da3c23d8ad5508bd1904dcd0e51962172ea6042
cairomedeiros <cairomedeiros13@gmail.com>
2024-09-26 22:07:52 -0300

    fix routes

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
-urlpatterns = [path('', views.index, name='index'),]
+router = DefaultRouter()
+router.register(r'pandas', views.PandasViewSet, basename='PandasViewSet')
+urlpatterns = router.urls
--- api/views.py
+++ api/views.py
@@ -5,6 +5,8 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 class PandasViewSet(viewsets.ViewSet):
-    def create(self, request):
+    def list(self, request):
         db, client = MongoConnection.get_db_handle("teste")
+        print(client)
+        print(db)
         return Response("<h1>Hello World!</h1>", status=status.HTTP_200_OK)
