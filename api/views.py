47aa67150deb54949679bf50a96ec457c799b613
cairomedeiros <cairomedeiros13@gmail.com>
2024-10-01 19:18:58 -0300

    just testing

--- api/views.py
+++ api/views.py
@@ -20,6 +20,13 @@ class PandasViewSet(viewsets.ViewSet):
         if excel_serializer.is_valid():
             valid_excel = excel_serializer.validated_data
             df = pd.read_excel(valid_excel["document"], engine='openpyxl')
+            df.index = [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+            # using Loc and Iloc
+            # print(df.loc[0])
+            # print(df.iloc[0])
+
+            print(df["Idade"].max())
+            print(df[df["Idade"] == df["Idade"].max()])
             return Response("<h1>Hello World!</h1>", status=status.HTTP_200_OK)
         else:
             return Response({"error": "Serializer is not valid"}, status=status.HTTP_400_BAD_REQUEST)
